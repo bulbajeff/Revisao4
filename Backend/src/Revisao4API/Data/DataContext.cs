6e43203439617d814cd045457f7b4fc0b7d69da4
Jefferson Sambuichi <samuraijeff@outlook.com>
2023-04-24 12:59:52 -0300

    revisao4api

---
+++ Backend/src/Revisao4API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Revisao4API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
